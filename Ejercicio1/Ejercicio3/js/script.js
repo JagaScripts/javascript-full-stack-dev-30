c59ad1ca9e25c36492510bcc48c43bdc9ee0f8f9
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-06-02 05:59:09 +0200

    Ejercicio 3 terminado y agregado xD

---
+++ Ejercicio1/Ejercicio3/js/script.js
@@ -0,0 +1,8 @@
+//Creamos un array de strings con los meses
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+//Recorremos la array
+meses.forEach((mes) => {
+    // mostramos el resultado por consola 
+    console.log(mes);
+})
