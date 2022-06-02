53ebc09756f42b1d4cde8629541763e9f621502e
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-06-02 05:52:54 +0200

    Ejercicio 2 terminado

--- Ejercicio1/js/codigo.js
+++ Ejercicio1/js/codigo.js
@@ -1,7 +1,5 @@
-//Funcionalidad del archivo anterior
+//Misma funcionalidad
 console.log("Hola Mundo");
 
-//Declaramos y asignamos valores a la variable mensaje
-const mensaje = "soy el primer script";
 //Mostramos el mensaje "Soy el primer script"
-window.alert(mensaje);
+window.alert("soy el primer script");
